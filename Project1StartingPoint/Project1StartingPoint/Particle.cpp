b0a2f69b796b6fe1917a7839c746ee98dc047587
Diego Bastos <bastosdiegol@gmail.com>
2022-09-28 15:15:51 -0400

    Particle class comments and operator fix

--- Project1StartingPoint/Project1StartingPoint/Particle.cpp
+++ Project1StartingPoint/Project1StartingPoint/Particle.cpp
@@ -8,6 +8,7 @@
 #define DEBUG_PRINT(x)
 #endif
 
+// Default Constructor
 Particle::Particle()
 	: position(0.0f)
 	, velocity(0.0f)
@@ -19,20 +20,30 @@ Particle::Particle()
 	DEBUG_PRINT("Particle::Particle();\n");
 }
 
+// Destructor
 Particle::~Particle() {
 	DEBUG_PRINT("Particle::~Particle();\n");
 }
 
+// Overloaded Constructor
 Particle::Particle(const Particle& particle) {
 	DEBUG_PRINT("Particle::Particle(const Particle& particle);\n");
+	this->position		= particle.position;
+	this->velocity		= particle.velocity;
+	this->acceleration	= particle.acceleration;
+	this->damping		= particle.damping;
+	this->mass			= particle.mass;
+	this->age			= particle.age;
 }
 
+// Overloaded Copy Operator
 Particle Particle::operator=(const Particle& particle) {
 	DEBUG_PRINT("Particle::operator=();\n");
 
 	return Particle();
 }
 
+// Method to print Particle info on the console
 void Particle::PrintInfo() {
 
 	DEBUG_PRINT("-----------------\n");
@@ -41,6 +52,7 @@ void Particle::PrintInfo() {
 
 }
 
+// Method to Integrate the Particle on a specified Delta Time
 void Particle::Integrate(float dt) {
 	//velocity = velocity + acceleration * dt;
 	//velocity *= damping;
@@ -49,7 +61,9 @@ void Particle::Integrate(float dt) {
 	velocity *= damping;
 	position = position + (velocity * dt);
 
+	// Aging the Particle
 	age -= dt;
+
 	// In case the particle hit the ground
 	// Set age to 0 to kill it
 	if (position.y <= 0.f) {
--- Project1StartingPoint/Project1StartingPoint/Particle.h
+++ Project1StartingPoint/Project1StartingPoint/Particle.h
@@ -5,11 +5,16 @@
 class Particle {
 	// Private area
 public:
+	// Default Constructor
 	Particle();
+	// Destructor
 	~Particle();
+	// Overloaded Constructor
 	Particle(const Particle& particle);
+	// Overloaded Copy Operator
 	Particle operator=(const Particle& particle);
 
+	// Variables of the Particle
 	glm::vec3 position;
 	glm::vec3 velocity;
 	glm::vec3 acceleration;
@@ -18,6 +23,8 @@ public:
 	float damping;
 	float mass;					// f = ma
 
+	// Method to print Particle info on the console
 	void PrintInfo();
+	// Method to Integrate the Particle on a specified Delta Time
 	void Integrate(float dt);
 };
